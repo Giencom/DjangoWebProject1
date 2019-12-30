02b553705cbe346d723520ac3d5883d8390ea169
Akiva Friedman <akivaf@npcglobal.com>
2019-12-30 14:36:13 -0500

    er

--- HelloDjangoApp/views.py
+++ HelloDjangoApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from datetime import datetime
 
 def index(request):
     now = datetime.now()
@@ -10,6 +11,7 @@ def index(request):
         "HelloDjangoApp/index.html",  # Relative path from the 'templates' folder to the template file
         # "index.html", # Use this code for VS 2017 15.7 and earlier
         {
-            'content': "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
+            'message': "Hello Django!", 
+            'content': "on " + now.strftime("%A, %d %B, %Y at %X")
         }
     )
