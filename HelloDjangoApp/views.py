8a6b787927974a98cc697ec8ec346296f34fa1fb
Akiva Friedman <akivaf@npcglobal.com>
2019-12-30 13:39:13 -0500

    No idea what this means

--- HelloDjangoApp/views.py
+++ HelloDjangoApp/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("Hello, Django!")
+    now = datetime.now()
+
+    return render(
+        request,
+        "HelloDjangoApp/index.html",  # Relative path from the 'templates' folder to the template file
+        # "index.html", # Use this code for VS 2017 15.7 and earlier
+        {
+            'content': "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
