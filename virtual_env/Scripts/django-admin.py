55b16a527cdd764ed2481c4dbc7cf930bd900fce
Akiva Friedman <Akiva Friedman@np.local>
2019-12-30 12:44:46 -0500

    Add project files.

---
+++ virtual_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ykovacs\source\repos\DjangoWebProject1\virtual_env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
